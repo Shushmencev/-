29b890b40fb36912dbefec923a78eedfaa54ae2b
Shushmencev97 <Shushmencev97@mail.ru>
2017-04-20 08:54:23 +0700

    commit 3

--- first.cpp
+++ first.cpp
@@ -11,14 +11,21 @@
 //Пространство имён:
 using namespace std;
 
+//Макросы:
+#define MAX_VALUE_RAND 100 + 1
+#define COUNT_ELEMENTS 100
+
 int main (){
 	
+	//Объявление множества:
 	set <int> mySet;
 	
+	//Заполнение множества случайными целыми числами:
 	srand(time(NULL));
-	for (int i = 0; i < 10; i++){
-		cout << rand() % 101 << endl;
+	for (int i = 0; i < COUNT_ELEMENTS; i++){
+		mySet.insert(rand() % MAX_VALUE_RAND);
 	}
 	
+	//Возврат нулевого значения:
 	return 0;
 }
